5b80e8ff7b4618101087d0b43d3dad84835732e8
Rishi Raj <kvkv.raj83@gmail.com>
2024-01-25 22:11:47 +0530

    changes to test tokens

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import Login from "./components/Login";
+import { RouterProvider, createBrowserRouter  } from "react-router-dom";
+import Login from "./components/Login.jsx";
+import DashBoard from "./components/ListDashboard";
+
 
 
 function App() {
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Login/>
+  },
+  {
+    path:'/Dashboard',
+    element:<DashBoard/>
+  }
+])
+
   return (
     <div className="App">
-      <Login/>
+    <RouterProvider router={router}/>
     </div>
   );
 }
--- src/components/ListDashboard.jsx
+++ src/components/ListDashboard.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 const DashBoard = () => {
+
+    useEffect(() => {
+        const storedAccessToken = localStorage.getItem('access_token');
+        const storedRefreshToken = localStorage.getItem('refresh_token');
+
+        console.log({
+            'access': storedAccessToken,
+            'refresh': storedRefreshToken
+        });
+    })
+
+
    return(
     <div>DashBoard</div>
    )
